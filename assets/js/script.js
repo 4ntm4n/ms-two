e5bea0efbb4d0d4be1a3963ee40d7326bce43ed3
Anton Askling <anton.askling@gmail.com>
2022-05-30 05:17:40 +0200

    add restore button inside renderNotes if note.removed = true

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,7 +48,7 @@ const noteMaker = (title, content, prio, _id, removed = false) => {
     note.id = nonPrioCount;
     myNotes.push(note);
   }
-  
+
   interact();
   renderNotes(notRemovedFilter(myNotes));
 };
@@ -58,10 +58,12 @@ const renderNotes = (array) => {
   const notesList = document.getElementById("notes-list");
   notesList.innerHTML = "";
 
-  for (let i = 0; i < array.length; i++) {  
+  for (let i = 0; i < array.length; i++) {
     li = document.createElement("li"); // create a list element for each object in array.
     li.classList.add("note");
     li.setAttribute("id", array[i]._id);
+    //add buttons here:
+
     li.innerHTML = `
             <h3 class="note-title" >${array[i].title}</h3>
             <p class="note-content">${array[i].content}</p>
@@ -69,26 +71,31 @@ const renderNotes = (array) => {
         `;
     //remove btn
     const rmBtn = document.createElement("button");
-    rmBtn.classList.add('rm-btn')
-    if (array[i].removed){
-      rmBtn.innerHTML = "DELETE"
-      li.classList.add('removed');
-    }else {
+    rmBtn.classList.add("rm-btn");
+    if (array[i].removed) {
+      rmBtn.innerHTML = "DELETE";
+      li.classList.add("removed");
+    } else {
       rmBtn.innerHTML = "Remove";
     }
 
-    
+    // add hidden restore button and display block in css 2, add sort button in html
+    const restBtn = document.createElement("button");
+    restBtn.classList.add("rest-btn");
+    if (array[i].removed) {
+      restBtn.innerHTML = "restore";
+      li.appendChild(restBtn);
+    }
+
     //important button
     const impNoteBtn = document.createElement("button");
-    impNoteBtn.classList.add('imp-btn')
-    if (array[i].prio){
-      impNoteBtn.innerHTML = "not prio"
-      li.classList.add('prio');
-    }else {
+    impNoteBtn.classList.add("imp-btn");
+    if (array[i].prio) {
+      impNoteBtn.innerHTML = "not prio";
+      li.classList.add("prio");
+    } else {
       impNoteBtn.innerHTML = "make prio";
     }
-  
-    
 
     li.appendChild(rmBtn);
     li.appendChild(impNoteBtn);
@@ -97,6 +104,10 @@ const renderNotes = (array) => {
     //event listeners to buttons inside the notes
     rmBtn.addEventListener("click", (i) => {
       removeNote(i);
+    });
+
+    restBtn.addEventListener("click", (i) => {
+      restoreNote(i);
     });
 
     impNoteBtn.addEventListener("click", (i) => {
@@ -116,13 +127,41 @@ const removeNote = (i) => {
 
   //is note already removed? delete it! else set removed to true.
   if (myNotes[index].removed === true) {
-    myNotes.splice([index], 1);
-    renderNotes(removedFilter(myNotes)); //render removed notes
+    if (window.event.ctrlKey) {
+      // nested else if to restore removed
+      const domNoteId = Number(i.target.parentNode.id);
+
+      //find the clicked note in the myNotes array by filtering its id.
+      const index = myNotes.findIndex((note) => {
+        return note._id === domNoteId;
+      });
+
+      if (myNotes[index].removed) {
+        myNotes[index].removed = false;
+        renderNotes(removedFilter(myNotes));
+      }
+    } else {
+      myNotes.splice([index], 1);
+      renderNotes(removedFilter(myNotes)); //render removed notes
+    }
   } else {
     myNotes[index].removed = true;
     renderNotes(notRemovedFilter(myNotes)); //render all notes - removed
   }
-}
+};
+
+const restoreNote = (i) => {
+  const domNoteId = Number(i.target.parentNode.id);
+
+  //find the clicked note in the myNotes array by filtering its id.
+  const index = myNotes.findIndex((note) => {
+    return note._id === domNoteId;
+  });
+
+  myNotes[index].removed = false;
+
+  renderNotes(removedFilter(myNotes));
+};
 
 //function to make note important or non important.
 const NoteImpStatus = (i) => {
@@ -133,22 +172,19 @@ const NoteImpStatus = (i) => {
     return note._id === domNoteId;
   });
 
-   let tempArr = [];
+  let tempArr = [];
   if (myNotes[index].prio === true) {
     myNotes[index].prio = false; //set prio to false
     tempArr = myNotes.splice([index], 1);
-    myNotes = myNotes.concat(tempArr)
+    myNotes = myNotes.concat(tempArr);
   } else {
     myNotes[index].prio = true;
     tempArr = myNotes.splice([index], 1);
-    myNotes = tempArr.concat(myNotes)
-  }; 
+    myNotes = tempArr.concat(myNotes);
+  }
 
   renderNotes(notRemovedFilter(myNotes));
-}
-
-
-
+};
 
 //function that compares two arrays and find index of their matches
 const findMatch = (baseArr, compArr) => {
@@ -172,12 +208,6 @@ const findMatch = (baseArr, compArr) => {
   return indexInBaseArr;
 };
 
-const deleteNoteObj = (baseArr, index) => {
-  console.log(index);
-  console.log(baseArr);
-  baseArr.splice(index, 1);
-  console.log(baseArr);
-};
 
 //create function to remove duplicates in array.
 
@@ -190,6 +220,7 @@ const deleteDuplies = (array) => {
   return filtered;
 };
 
+//filters that render different types of notes obj. based on obj. keys.
 const impFilter = (array) => {
   return array.filter((note) => {
     return note.prio === true;
@@ -209,14 +240,13 @@ const notRemovedFilter = (array) => {
 };
 
 //add function to sort notes by name A-Z.
-
 const sortByTitle = (array) => {
+
   // note.sorted has note been added? add it, set it to false and continue
   if (array[0].sorted === null) {
     array.forEach((note) => (note.sorted = false));
   }
-
-  /* is note.sorted false? sort note.title A-Z set .sorted to true and renderNotes, 
+  /* is note.sorted false? sort note.title A-Z, set .sorted to true and renderNotes, 
   else sort .title Z-A set .sorted to false and render notes */
   if (array[0].sorted === false) {
     array.sort((a, b) => (a.title > b.title ? -1 : 1));
@@ -229,19 +259,21 @@ const sortByTitle = (array) => {
   }
 };
 
-
 const interact = () => {
   const numOfNotes = myNotes.length;
-  const h2 = document.getElementsByTagName('h2')[0];
+  const h2 = document.getElementsByTagName("h2")[0];
 
-    numOfNotes == 1 ? h2.innerHTML = "You have added a note, therefore you are..."
-  : numOfNotes == 2 ? h2.innerHTML = "yeah... that's pretty much it..."
-  : numOfNotes == 100 ? h2.innerHTML = "All your notes are belong to us!"
-  : numOfNotes >= 999 ? h2.innerHTML = "mother of god.. no one has created this many notes... bailing out, your on your own.... "
-  : h2.innerHTML = "All your notes are displayed here";
-  
-
-}
+  numOfNotes == 1
+    ? (h2.innerHTML = "You have added a note, therefore you are...")
+    : numOfNotes == 2
+    ? (h2.innerHTML = "yep that's it...")
+    : numOfNotes == 100
+    ? (h2.innerHTML = "All your notes are belong to us!")
+    : numOfNotes >= 999
+    ? (h2.innerHTML =
+        "mother of god.. no one has created this many notes... bailing out, your on your own.... ")
+    : (h2.innerHTML = "All your notes are displayed here");
+};
 
 //get control panel buttons from index.html
 const delBtn = document.getElementById("render-deleted");
@@ -263,38 +295,3 @@ oldBtn.addEventListener("click", () => renderNotes(notRemovedFilter(myNotes)));
 const form = document.getElementById("notes-input");
 form.addEventListener("submit", handleSubmit);
 
-
-/*   //important button
-    const impNoteBtn = document.createElement("button");
-    impNoteBtn.classList.add('imp-btn')
-
-    if (array[i].prio) {
-      li.style.backgroundColor = 'orange'; //make note bg grey when removed
-      impNoteBtn.innerHTML = "not prio" // change button text from remove to delete when removed
-
-      }else {
-        impNoteBtn.innerHTML = "make prio"
-      }
-      li.appendChild(impNoteBtn);
-
-         //remove btn
-    const rmBtn = document.createElement("button");
-    rmBtn.classList.add('rm-btn')
-
-     if (array[i].removed && !array[i].prio) {
-        li.style.backgroundColor = 'grey'; //make note bg grey when removed
-        rmBtn.innerHTML = "DELETE" // change button text from remove to delete when removed
-      }else  if (array[i].removed && array[i].prio) {
-        li.style.backgroundColor = 'grey'; //make note bg grey when removed
-       rmBtn.innerHTML = "DELETE" // change button text from remove to delete when removed
-      } else {
-
-       rmBtn.innerHTML = "Remove";
-      }
-    
-      li.appendChild(rmBtn);
-    
-      
-
-  
-    notesList.appendChild(li); */
